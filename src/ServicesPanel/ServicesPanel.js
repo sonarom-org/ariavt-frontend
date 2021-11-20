cd9d482a6f88b7166bf297ce57f15b96e30ab3d8
José Morano <j.morano@udc.es>
2021-11-20 19:04:18 +0100

    Show services and the results provided by them for a given image. [Closes #27]

--- src/ServicesPanel/ServicesPanel.js
+++ src/ServicesPanel/ServicesPanel.js
@@ -1,51 +1,12 @@
-import axios from "axios";
 
 import AddServiceForm from "./AddServiceForm";
 import EditServiceForm from "./EditServiceForm";
 import ItemsPanel from "../Panel/ItemsPanel";
-import { getToken } from "../authentication/authentication";
-import config from "../config.json";
 import ServiceItem from "./ServiceItem";
+import getAllServices from "./services";
 
 
 export default function ServicesPanel(props) {
-
-  function getAllServices(setItems) {
-    const token = getToken();
-    if (!token) {
-      return;
-    }
-    axios.get(config.API_URL+"/services",
-      {
-        headers: {
-          'Authorization': `Bearer ${token}`,
-          'Content-Type': 'application/json'
-        }
-      }
-    ).then(response => {
-      console.log(response.data);
-
-      for (const service of response.data) {
-        setItems(services => ({
-          ...services,
-          [service.id]: {
-            id: service.id,
-            name: service.name,
-            url: service.url,
-            fullName: service.full_name,
-            description: service.description,
-            resultType: service.result_type,
-          }
-        }));
-      }
-    }).catch(error => {
-      if (error.response.status === 401) {
-        // setError(error.response.data.message);
-      } else {
-        // setError("Something went wrong. Please try again later.");
-      }
-    });
-  }
 
   return <ItemsPanel
     itemType="service"
